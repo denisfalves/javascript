b29c7fd6f25925c765b9e9bf518a3b763616b90a
denisfalves <denisx_@hotmail.com>
2022-09-28 16:00:01 -0300

    condicional

--- aula11 - Condições/ex009.js
+++ aula11 - Condições/ex009.js
@@ -1,6 +1,7 @@
-var vel = 60.5
-console.log(`A velocidade do seu carro é ${vel}km/h`)
-if (vel > 60) {
-    console.log(`Você ultrapasswou a velocidade permitida. MULTADO!`)
+var país ='EUA'
+console.log(`Vivendo em ${país}`)
+if (país != 'Brasil') {
+    console.log('Você é Estrangeiro!')
+} else {
+    console.log('Você é Brasileiro!')
 }
-console.log(`Dirija sempre usando cinto de segurança!`)
